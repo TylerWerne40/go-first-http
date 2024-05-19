0deb613a6019c9daf509cabde6e9487a1ef0dea3
Tyler <TylerWerne40@gmail.com>
2024-05-18 18:52:29 -0600

    First commit.

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"net"
+)
+
+func server() {
+	// listen on a port
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		// accept a conneciton
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// handle the connection
+		go handleServerConnection(c)
+	}
+}
+
+func handleServerConnection(c net.Conn) {
+	// receiver the message
+	var msg string
+	err := gob.NewDecoder(c).Decode(&msg)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(msg)
+	}
+
+	c.Close()
+}
+
+func client() {
+	// connect to the server
+	c, err := net.Dial("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer c.Close()
+	msg := "Hello World!"
+	fmt.Println("Sending: ", msg)
+	err = gob.NewEncoder(c).Encode(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func main() {
+	go server()
+	go client()
+
+	var input string
+	fmt.Scanln(&input)
+}
