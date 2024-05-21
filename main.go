723de81a3012d0dde65e1f0d0a6c9842f5934652
Tyler <TylerWerne40@gmail.com>
2024-05-21 09:17:02 -0600

    http

--- main.go
+++ main.go
@@ -1,75 +1,26 @@
 package main
 
 import (
-	"encoding/gob"
-	"fmt"
-	"net"
+	"io"
+	"net/http"
 )
 
-func server() {
-	// listen on a port
-	ln, err := net.Listen("http", ":8080")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		// accept a connection
-		c, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		// handle the connection
-		go handleServerConnection(c)
-	}
-}
-
-func handleServerConnection(c net.Conn) {
-	// receiver the message
-	var msg string
-	err := gob.NewDecoder(c).Decode(&msg)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(msg)
-	}
-
-	err = c.Close()
-	if err != nil {
-		return
-	}
-}
-
-func client() {
-	// connect to the server
-	c, err := net.Dial("http", ":8080")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer func(c net.Conn) {
-		err := c.Close()
-		if err != nil {
-
-		}
-	}(c)
-	msg := "Hello World!"
-	fmt.Println("Sending: ", msg)
-	err = gob.NewEncoder(c).Encode(msg)
-	if err != nil {
-		fmt.Println(err)
-	}
-
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	io.WriteString(res,
+		`<doctype html>
+	<html>
+		<head>
+			<title>Hello World!</title>
+		</head>
+		<body>
+			Hello World
+		</body>
+	</html>`,
+	)
 }
 
 func main() {
-	go server()
-	go client()
-
-	var input string
-	_, err := fmt.Scanln(&input)
-	if err != nil {
-		return
-	}
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
 }
